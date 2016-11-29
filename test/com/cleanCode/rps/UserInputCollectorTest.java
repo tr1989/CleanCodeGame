7aabc95c93cffdad95c4d027c33ac95dd5a98c8b
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-30 01:59:53 +0530

    Use proper assert operations

--- test/com/cleanCode/rps/UserInputCollectorTest.java
+++ test/com/cleanCode/rps/UserInputCollectorTest.java
@@ -2,23 +2,20 @@ package com.cleanCode.rps;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class UserInputCollectorTest {
     @Test
     public void testInputValidatorForUserPositive(){
         UserInputCollector userInputCollector = new UserInputCollector("S");
-        boolean actual = true;
         boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput());
-        Assert.assertEquals(actual, expected);
+        Assert.assertTrue(expected);
     }
 
     @Test
     public void testInputValidatorForUserNegative(){
         UserInputCollector userInputCollector = new UserInputCollector("E");
-        boolean actual = false;
         boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput());
-        Assert.assertEquals(actual, expected);
+        Assert.assertFalse(expected);
     }
 
 }
