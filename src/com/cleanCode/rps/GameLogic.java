85c7179d6a4ab1f382e3508a5bbe981fb816e498
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-29 23:42:34 +0530

    Added code for Rock and Paper test case

--- src/com/cleanCode/rps/GameLogic.java
+++ src/com/cleanCode/rps/GameLogic.java
@@ -3,7 +3,18 @@ package com.cleanCode.rps;
 public class GameLogic{
 
     public String decideWinner(String userInput1, String userInput2){
-
+        if(userInput1.equalsIgnoreCase(String.valueOf(Constants.Inputs.R))){
+            return compareInputWithRock(userInput2);
+        }
         return "true";
+    }
+
+    public String compareInputWithRock(String userInput){
+        if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.P))) {
+            return Constants.Results.PAPER_WINS.toString();
+        } else if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.S))) {
+            return Constants.Results.ROCK_WINS.toString();
+        }
+        return Constants.Results.TIE.toString();
     }
 }
