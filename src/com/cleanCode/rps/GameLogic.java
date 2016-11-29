ae44a1ab6da018f933e3019ee07d6bfd8fdf73bd
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-29 23:56:30 +0530

    Added code for handling scenarios incase first input is Paper

--- src/com/cleanCode/rps/GameLogic.java
+++ src/com/cleanCode/rps/GameLogic.java
@@ -5,8 +5,11 @@ public class GameLogic{
     public String decideWinner(String userInput1, String userInput2){
         if(userInput1.equalsIgnoreCase(String.valueOf(Constants.Inputs.R))){
             return compareInputWithRock(userInput2);
+        }else if(userInput1.equalsIgnoreCase(String.valueOf(Constants.Inputs.P))){
+            return compareInputWithPaper(userInput2);
+        } else {
+            return compareInputWithScissors(userInput2);
         }
-        return "true";
     }
 
     public String compareInputWithRock(String userInput){
@@ -14,6 +17,24 @@ public class GameLogic{
             return Constants.Results.PAPER_WINS.toString();
         } else if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.S))) {
             return Constants.Results.ROCK_WINS.toString();
+        }
+        return Constants.Results.TIE.toString();
+    }
+
+    public String compareInputWithPaper(String userInput){
+        if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.R))) {
+            return Constants.Results.PAPER_WINS.toString();
+        } else if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.S))) {
+            return Constants.Results.SCISSORS_WINS.toString();
+        }
+        return Constants.Results.TIE.toString();
+    }
+
+    public String compareInputWithScissors(String userInput){
+        if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.R))) {
+            return Constants.Results.ROCK_WINS.toString();
+        } else if(userInput.equalsIgnoreCase(String.valueOf(Constants.Inputs.P))) {
+            return Constants.Results.SCISSORS_WINS.toString();
         }
         return Constants.Results.TIE.toString();
     }
