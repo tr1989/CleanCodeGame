affafb3cc280d561b122cbb9a6e577640ec87ed1
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-29 22:23:55 +0530

    Added testcases for UserInputCollector

--- test/com/cleanCode/rps/UserInputCollectorTest.java
+++ test/com/cleanCode/rps/UserInputCollectorTest.java
@@ -20,4 +20,20 @@ public class UserInputCollectorTest {
         boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput1());
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void testInputValidatorForUserTwoPositive(){
+        UserInputCollector userInputCollector = new UserInputCollector("S", "R");
+        boolean actual = true;
+        boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput2());
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testInputValidatorForUserTwoNegative(){
+        UserInputCollector userInputCollector = new UserInputCollector("S", "X");
+        boolean actual = false;
+        boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput2());
+        Assert.assertEquals(actual, expected);
+    }
 }
