2e8b68ad570d05f076421174d89046614c3b3818
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-29 23:45:11 +0530

    Added remaining test case incase first input is Rock

--- test/com/cleanCode/rps/GameLogicTest.java
+++ test/com/cleanCode/rps/GameLogicTest.java
@@ -17,4 +17,28 @@ public class GameLogicTest {
         String expected = gameLogic.decideWinner(userInput1.getUserInput(), userInput2.getUserInput());
         Assert.assertEquals(actual, expected);
     }
+
+    @Test
+    public void testGameLogicForRockAndScissors(){
+        UserInputCollector userInput1 = new UserInputCollector("R");
+        UserInputCollector userInput2 =  new UserInputCollector("S");
+
+        GameLogic gameLogic = new GameLogic();
+
+        String actual = String.valueOf(Constants.Results.ROCK_WINS);
+        String expected = gameLogic.decideWinner(userInput1.getUserInput(), userInput2.getUserInput());
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testGameLogicForRockAndRock(){
+        UserInputCollector userInput1 = new UserInputCollector("R");
+        UserInputCollector userInput2 =  new UserInputCollector("R");
+
+        GameLogic gameLogic = new GameLogic();
+
+        String actual = String.valueOf(Constants.Results.TIE);
+        String expected = gameLogic.decideWinner(userInput1.getUserInput(), userInput2.getUserInput());
+        Assert.assertEquals(actual, expected);
+    }
 }
