6538da06da95d097fc741c3b36d40df78e48a786
Rohitash Tulyani <rtulyani@sapient.com>
2016-11-29 22:21:19 +0530

    Postive and Negative test for user one input validation

--- test/com/cleanCode/rps/UserInputCollectorTest.java
+++ test/com/cleanCode/rps/UserInputCollectorTest.java
@@ -6,9 +6,17 @@ import static org.junit.Assert.*;
 
 public class UserInputCollectorTest {
     @Test
-    public void testInputValidatorForUserOne(){
-        UserInputCollector userInputCollector = new UserInputCollector("E", "R");
+    public void testInputValidatorForUserOnePositive(){
+        UserInputCollector userInputCollector = new UserInputCollector("S", "R");
         boolean actual = true;
+        boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput1());
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testInputValidatorForUserOneNegative(){
+        UserInputCollector userInputCollector = new UserInputCollector("E", "R");
+        boolean actual = false;
         boolean expected = userInputCollector.isInputValid(userInputCollector.getUserInput1());
         Assert.assertEquals(actual, expected);
     }
